0b539255288fac2360a0a89eff937bbb2ff7f1c4
Evgeniy Dashuk <evg.das@gmail.com>
2020-08-03 08:20:14 +0200

    fix error

--- src/test/java/pastebin/PasteBinTests.java
+++ src/test/java/pastebin/PasteBinTests.java
@@ -1,29 +1,31 @@
 package pastebin;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.commands.PressEnter;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PasteBinTests {
     @Test()
-
     void pasteTextToForm() {
         Configuration.holdBrowserOpen = true;
         open("https://pastebin.com/");
         $(byId("postform-text")).setValue("Hello from QAGURU");
-       //не работает, скрыт список
-        $(byId("postform-expiration")).selectOption("10 Minutes");
+
+        executeJavaScript("$('#postform-expiration').attr('style','display: block;')");
+        $("#postform-expiration").selectOption("10 Minutes");
+
+
         $(byXpath("//button[text()='Create New Paste']")).click();
 
-        //title не срабатывает?
-     //   $("title").shouldHave(exactText("Hello from QAGURU"));
+        sleep(2000);
+        assertEquals(title(), "Hello from QAGURU" + " - Pastebin.com");
 
-        //   $(byXpath("/html/body/div[1]/div[2]/div[1]/div[2]/div[3]/div[2]/ol/li/div")).shouldHave(exactText("Hello from QAGURU"));
+
     }
 }
