077e5bd56f0b725e783f08614d0a02f8748bba6a
Evgeniy Dashuk <evg.das@gmail.com>
2020-08-10 08:02:19 +0200

    some fix

--- src/test/java/alfabank/AlfaBankTests.java
+++ src/test/java/alfabank/AlfaBankTests.java
@@ -4,25 +4,34 @@ import basetest.BaseTest;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AlfaBankTests extends BaseTest {
-    final String urlAlfaBank="https://alfabank.ru/";
+    final String urlAlfaBank = "https://alfabank.ru/";
+
     @Test
     void checkSizeDepozit() {
         open(urlAlfaBank);
-        $(byText("Вклады")).click();
-        $(byText("Депозиты")).click();
-        $$(".product-cell__cell-back").filter(visible).shouldHaveSize(2);
+
+        $(byTitle("Вклады")).click();
+        $(byTitle("Депозиты")).click();
+        $(byText("Архивные депозиты")).click();
+
+        $$((".product-cell__cell-back")).shouldHaveSize(3);
     }
 
     @Test
     public void openPageDepozitInsurance() {
         open(urlAlfaBank);
-        $(byText("Вклады")).click();
-        $(".selected").sibling(4).$("a").click();
-        $("h1").shouldHave(text("Страхование вкладов"));
-        $(".col-sm-8").parent().shouldHave(text("АО «АЛЬФА-БАНК» является участником"));
+
+        $(byTitle("Вклады")).click();
+        $(".selected").sibling(4).click();
+
+        //ниже не работает почему то, буду разбираться.
+      //  $(byTitle("Страхование вкладов")).parent().parent().sibling(4).click();
+
+       $("h1").shouldHave(text("Страхование вкладов"));
+       $(".col-sm-8").shouldHave(text("АО «АЛЬФА-БАНК» является участником"));
     }
 }
