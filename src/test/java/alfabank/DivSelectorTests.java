978f41746940be5c609dd82617bd96cff9e715f2
Vasenkov Stanislav <aanher@gmail.com>
2020-08-27 22:20:31 +0300

    Update and rename divSelectorTests.java to DivSelectorTests.java
    Классы в Java должны начинаться с заглавной буквы

--- src/test/java/alfabank/DivSelectorTests.java
+++ src/test/java/alfabank/DivSelectorTests.java
@@ -8,10 +8,11 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class divSelectorTests extends BaseTest {
+public class DivSelectorTests extends BaseTest {
     @Test
     public void checkDiv() {
         open("file:/home/evgeniy/IdeaProjects/qaguru/src/test/resources/divtest.html");
+        
         $("div h1").shouldHave(text("Text1"));
         $("div").$("h1").shouldHave(text("Text1"));
         //не нашел разницы, может на сложном форматировании есть...
